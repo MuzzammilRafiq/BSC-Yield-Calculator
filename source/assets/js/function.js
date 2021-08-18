a8009da4f8ea9a0e25c0be498ab1afe70276c537
MoonWillionaire <moonwillionaire@gmail.com>
2021-08-18 19:25:41 +0300

    Update function.js

--- source/assets/js/function.js
+++ source/assets/js/function.js
@@ -7,24 +7,33 @@ dots: true,
 infinite: true,
 speed: 800,
 autoplay: true,
-autoplaySpeed: 3000,
-slidesToShow: 3,
-slidesToScroll: 3,
-responsive: [
-{
-breakpoint: 1024,
-settings: {
+autoplaySpeed: 4000,
 slidesToShow: 4,
 slidesToScroll: 4,
+responsive: [
+{
+breakpoint: 1324,
+settings: {
+slidesToShow: 3,
+slidesToScroll: 3,
 infinite: true,
 dots: true
 }
 },
 {
-breakpoint: 600,
+breakpoint: 1024,
 settings: {
 slidesToShow: 2,
-slidesToScroll: 2
+slidesToScroll: 2,
+infinite: true,
+dots: true
+}
+},
+{
+breakpoint: 700,
+settings: {
+slidesToShow: 1,
+slidesToScroll: 1
 }
 },
 {
@@ -34,14 +43,8 @@ slidesToShow: 1,
 slidesToScroll: 1
 }
 }
-,
-{
-breakpoint: 320,
-settings: {
-slidesToShow: 1,
-slidesToScroll: 1
-}
-}
+
+
 ]
 });
 	
