abb75bc123fd5f06061ed8427717dbd74811d07c
MoonWillionaire <moonwillionaire@gmail.com>
2021-08-18 19:22:19 +0300

    s

--- source/assets/js/function.js
+++ source/assets/js/function.js
@@ -7,15 +7,15 @@ dots: true,
 infinite: true,
 speed: 800,
 autoplay: true,
-autoplaySpeed: 2000,
-slidesToShow: 4,
-slidesToScroll: 4,
+autoplaySpeed: 3000,
+slidesToShow: 3,
+slidesToScroll: 3,
 responsive: [
 {
 breakpoint: 1024,
 settings: {
-slidesToShow: 3,
-slidesToScroll: 3,
+slidesToShow: 4,
+slidesToScroll: 4,
 infinite: true,
 dots: true
 }
@@ -34,7 +34,14 @@ slidesToShow: 1,
 slidesToScroll: 1
 }
 }
-
+,
+{
+breakpoint: 320,
+settings: {
+slidesToShow: 1,
+slidesToScroll: 1
+}
+}
 ]
 });
 	
